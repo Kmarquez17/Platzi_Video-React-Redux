6e9e8a6f9dbe155762b8e80d5b4c551bb5e7cbfb
Kevin <marquezkrodriguez24@gmail.com>
2018-03-16 17:33:02 -0600

    Estados de los Componentes y ciclo de vida de los componentes

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './media.css'
 class Media extends Component{
+  state = {
+    author: ''
+  }
   // constructor(props){
   //   super(props)
+  //   this.state = {
+  //     author: props.author,
+  //   }
   //   //(this.handleClick contexto del DOM).(bind(this) contexto de la clase)
-  //   this.handleClick = this.handleClick.bind(this)
+  //   //this.handleClick = this.handleClick.bind(this)
   // }
 
   handleClick = () => {
-      console.log(this.props.title);
+      //console.log(this.props.title);
+      this.setState({
+        author: 'Ricardo Celis',
+      })
   }
 
   render(){
@@ -33,7 +42,7 @@ class Media extends Component{
             className="Media-imagen"
           />
           <h3 className="Media-title">{this.props.title}</h3>
-          <p className="Media-author">{this.props.author}</p>
+          <p className="Media-author">{this.state.author}</p>
         </div>
       </div>
     )
@@ -49,3 +58,50 @@ Media.propTypes = {
 };
 
 export default Media;
+
+
+/*
+  --Ciclo de vida de un componentes
+    Los métodos de los Componentes son llamados en momentos específicos:
+
+  -----------------------------------------------------------------
+    Montado: El momento en que el componente entra en escena.
+  -----------------------------------------------------------------
+
+    ** Constructor: método llamado antes de que el componente sea montado (componente aun no se ve).
+      ->Podemos iniciar el estado
+      ->Enlazar eventos (bind).
+      ->Es el primer metodo que se llama al instanciar un componente.
+
+   ** componentWillMount: método llamado inmediatamente antes de que el componente se vaya a montar (componente aun no se ve).
+      ->Podemos hacer un setState()
+      ->No hacer llamados a un API o suscripción a eventos.
+
+    ** Render: método que contiene todos los elementos a renderizar (estructura del componente).
+      ->Contiene JSX en el return.
+      ->Puedes calcular propiedades nCompleto = name + lastName.
+
+    ** componentDidMount: Método llamado luego de montarse el componente (el componente ya esta en la pantalla).
+      ->Solo se lanza una vez.
+      ->Enlazar (bind) de eventos.
+      ->Es el primer método que se llama al instanciar un componente.
+      ->Aqui podemos utilizar APIs (Navegador o Datos Externos).
+
+  -----------------------------------------------------------------
+    Actualización
+  -----------------------------------------------------------------
+
+    ** componentWillReceiveProps: método llamado al recibir nuevas propiedades que sirve para actualizar el estado con base a las nuevas propiedades.
+    ** shouldComponentUpdate: método que condiciona si el componente se debe volver a renderizar, es utilizado para optimizar el rendimiento.
+    ** componentWillUpdate: método llamado antes de re-renderizar un componente, es utilizado para optimizar el rendimiento.
+    ** Render: método que realiza el re-render.
+    ** componentDidUpdate: método llamado luego del re-render.
+  -----------------------------------------------------------------
+    Desmontado
+  -----------------------------------------------------------------
+    **  componentWillUnmount: método llamado antes de que el componente sea retirado de la aplicación.
+  -----------------------------------------------------------------
+    Manejo de Errores
+  -----------------------------------------------------------------
+    ** componentDidCatch: método llamado cuando ocurre un error al renderizar el componente, el manejo de errores solamente ocurre en componentes hijos.
+*/
