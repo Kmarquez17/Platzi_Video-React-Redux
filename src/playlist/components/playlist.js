071777fa4dac1b49e2704590c6ef7c7e25c8fcb6
Kevin <marquezkrodriguez24@gmail.com>
2018-03-23 17:06:04 -0600

    Composicion de Componentes

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Media from './media.js'
+import Play from '../../icons/components/play'
+import Volumen from '../../icons/components/volumen'
+import Pause from '../../icons/components/pause'
 import './playlist.css'
 
-class Playlist extends Component{
-  render(){
-    const playlist = this.props.data.categories[0].playlist
-    console.log(this.props.data);
+function Playlist(props) {
+    const playlist = props.data.categories[0].playlist
+    console.log(props.data);
     return(
       <div className="Playlist">
+        <Play
+            size={30}
+            color="blue"
+        />
+        <Volumen
+            size={30}
+            color="red"
+        />
+        <Pause
+            size={30}
+            color="green"
+        />
         {
           playlist.map((item) => {
             return <Media {...item} key={item.id}/>
@@ -15,7 +29,39 @@ class Playlist extends Component{
         }
       </div>
     )
-  }
 }
-
 export default Playlist
+
+// import React, { Component } from 'react'
+// import Media from './media.js'
+// import './playlist.css'
+//
+//
+// class Playlist extends Component{
+//   render() {
+//     const { categories } = this.props.data
+//     return (
+//       <div>
+//         {
+//           categories.map(category =>
+//             <div key={category.id}>
+//               <h1>{category.title}</h1>
+//               <h3>{category.description}</h3>
+//               <div className="Playlist">
+//                 {
+//                   category.playlist.map(item =>
+//                     <Media
+//                       key={item.id}
+//                       {...item}
+//                     />)
+//                 }
+//               </div>
+//             </div>
+//           )
+//         }
+//       </div>
+//     )
+//   }
+// }
+//
+// export default Playlist
