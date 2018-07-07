23d5c364a0153a0004ee5c6b31469de2a8bb0e67
KMárquez <marquezkrodriguez24@gmail.com>
2018-07-07 16:14:19 -0600

    Middlewares y Añadiendo múltiples Middlewares

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-
 import { Provider } from 'react-redux';
-
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers/index';
 import { Map as map } from 'immutable';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import logger from 'redux-logger'
+// ----- versión pre-ES6
+// function logger({getState,dispatch}){
+//   return (next) => {
+//     return (action) => {
+//       console.log('Nuevo Viejo accion', getState().toJS())
+//       console.log('Enviar accion', action)
+//       const value = next(action)
+//       console.log('Nuevo Estado accion', getState().toJS())
+//       return value
+//     }
+//   }
+// }
+
+// --- versión ES6
+// const logger = ({ dispatch, getState }) => next => action => {
+// 	console.log( 'estado anterior:', getState().toJS() )
+// 	console.log( 'enviando acción:', action)
+// 	const rslt = next( action )
+// 	console.log( 'nuevo estado   :', getState().toJS() )
+// 	return rslt
+// }
 
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(
+    applyMiddleware(logger)
+  )
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const homeContainer = document.getElementById('home-container')
